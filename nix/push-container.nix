0de167b90f278b1e1277c94b0ec1f28cc7a5bf73
Arian van Putten <arian.vanputten@gmail.com>
2026-01-09 18:07:06 +0100

    Fix push untagged

--- nix/push-container.nix
+++ nix/push-container.nix
@@ -2,7 +2,6 @@
 writeShellApplication {
   name = "push-container";
   runtimeInputs = [ skopeo ];
-  runtimeEnv = { inherit (container) imageTag imageName };
   text = '' 
     ${container} | skopeo copy docker-archive:/dev/stdin "docker://ghcr.io/$${GITHUB_REPOSITORY}@@unknown-digest@@" --dest-username "$GITHUB_ACTOR" --dest-password "$GITHUB_TOKEN"
   '';
