a280ab346809952c57f564e20a9fbce3c040824d
Arian van Putten <arian.vanputten@gmail.com>
2026-01-09 18:04:55 +0100

    push untagged

--- nix/push-container.nix
+++ nix/push-container.nix
@@ -2,7 +2,8 @@
 writeShellApplication {
   name = "push-container";
   runtimeInputs = [ skopeo ];
+  runtimeEnv = { inherit (container) imageTag imageName };
   text = '' 
-    ${container} | skopeo copy docker-archive:/dev/stdin "docker://ghcr.io/$GITHUB_REPOSITORY" --dest-username "$GITHUB_ACTOR" --dest-password "$GITHUB_TOKEN"
+    ${container} | skopeo copy docker-archive:/dev/stdin "docker://ghcr.io/$${GITHUB_REPOSITORY}@@unknown-digest@@" --dest-username "$GITHUB_ACTOR" --dest-password "$GITHUB_TOKEN"
   '';
 }
