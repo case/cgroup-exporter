68b6fb2517eecc69152a534f57988fcfc6c8080f
Arian van Putten <arian.vanputten@gmail.com>
2026-01-09 18:29:55 +0100

    metadata

--- nix/container.nix
+++ nix/container.nix
@@ -3,9 +3,16 @@ dockerTools.streamLayeredImage {
   name = "ghcr.io/arianvp/cgroup-exporter";
   config = {
     Entrypoint = [ "${cgroup-exporter}/bin/cgroup-exporter" ];
-    Cmd = [ "-listen-address" ":13232" ];
+    Cmd = [
+      "-listen-address"
+      ":13232"
+    ];
     ExposedPorts."13232/tcp" = { };
     User = "65534:65534"; # nobody:nobody
     Volumes."/sys/fs/cgroup" = { };
+
+    Labels = {
+      "org.opencontainers.image.source" = "https://github.com/arianvp/cgroup-exporter";
+    };
   };
 }
