a1a213848e3fdb473b453e499180a8e225b2def5
Arian van Putten <arian.vanputten@gmail.com>
2026-01-09 19:28:32 +0100

    Revert "push tag"
    This reverts commit 0b14b766b21bf8679ed1b9afd8a8da4e46af7dd2.

--- nix/push-container.nix
+++ nix/push-container.nix
@@ -1,11 +1,8 @@
-{ writeShellApplication, skopeo, container, jq }:
+{ writeShellApplication, skopeo, container }:
 writeShellApplication {
   name = "push-container";
-  runtimeInputs = [ skopeo jq ];
-  runtimeEnv.conf = container.passthru.conf;
-  runtimeEnv.imageTag = container.passthru.imageTag;
-  text = ''
-    ${container} | skopeo copy docker-archive:/dev/stdin "docker://ghcr.io/''${GITHUB_REPOSITORY}:''${imageTag}" --dest-username "$GITHUB_ACTOR" --dest-password "$GITHUB_TOKEN" --digestfile digest
-    fi
+  runtimeInputs = [ skopeo ];
+  text = '' 
+    ${container} | skopeo copy docker-archive:/dev/stdin "docker://ghcr.io/''${GITHUB_REPOSITORY}@@unknown-digest@@" --dest-username "$GITHUB_ACTOR" --dest-password "$GITHUB_TOKEN"
   '';
 }
