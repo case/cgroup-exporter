3e8f98f6de11511b90d5cce6946d143e3e8fdb59
Arian van Putten <arian.vanputten@gmail.com>
2026-01-09 18:10:28 +0100

    Fix push untagged

--- nix/push-container.nix
+++ nix/push-container.nix
@@ -3,6 +3,6 @@ writeShellApplication {
   name = "push-container";
   runtimeInputs = [ skopeo ];
   text = '' 
-    ${container} | skopeo copy docker-archive:/dev/stdin "docker://ghcr.io/$${GITHUB_REPOSITORY}@@unknown-digest@@" --dest-username "$GITHUB_ACTOR" --dest-password "$GITHUB_TOKEN"
+    ${container} | skopeo copy docker-archive:/dev/stdin "docker://ghcr.io/''${GITHUB_REPOSITORY}@@unknown-digest@@" --dest-username "$GITHUB_ACTOR" --dest-password "$GITHUB_TOKEN"
   '';
 }
