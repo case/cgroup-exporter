a8bb45973e4cf256f84004c802299def0633a65b
Arian van Putten <arian.vanputten@gmail.com>
2026-02-10 22:59:20 +0100

    Fix bash

--- nix/push-container.nix
+++ nix/push-container.nix
@@ -4,6 +4,6 @@ writeShellApplication {
   runtimeInputs = [ skopeo ];
   runtimeEnv.imageTag = container.imageTag;
   text = ''
-    ${container} | skopeo copy docker-archive:/dev/stdin "docker://''${1}
+    ${container} | skopeo copy docker-archive:/dev/stdin "docker://''${1}"
   '';
 }
